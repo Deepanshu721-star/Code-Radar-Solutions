fd1318532a2f6ab7cfdb4a3996197556d816f2ab
Deepanshu721-star <deepanshu1063.becse24@chitkara.edu.in>
2025-01-31 16:46:20 +0530

    Solution to Grade Evaluation

---
+++ Grade Evaluation@@6788dd63490637cfc44e1bab/code.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+// #include <ctype.h>
+
+int main(){
+    char Light;
+    scanf("%c",&Light);
+    if(Light=='A'){
+        printf("Excellent");
+    }else if(Light=='B'){
+        printf("Good")
+    }else if(Light=='C'){
+        printf("Average");
+    }else if(Light=='D'){
+        printf("Below Average");
+    }else if(Light=='F'){
+        printf("Fail");
+    }else{
+        printf("Invalid input");
+    }
+    return 0;
+}
