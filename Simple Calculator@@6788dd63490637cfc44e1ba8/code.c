e397fdecf6078f5d578a2a1f3b21b52dfbfaaa28
Deepanshu721-star <deepanshu1063.becse24@chitkara.edu.in>
2025-01-31 17:13:11 +0530

    Solution to Simple Calculator

--- Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
+++ Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
@@ -1,9 +1,9 @@
 #include<stdio.h>
 int main(){
     int a,b;
-    scanf("%d %d",a,b);
+    scanf("%d %d",&a,&b);
     char op;
-    scanf("%c",op);
+    scanf("%c",&op);
     if(op=='+'){
         printf("%d",a+b);
     }else if(op=='-'){
